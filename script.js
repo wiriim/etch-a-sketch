b24717dc9cfae56b4996d47aac77e63cb93c7c3a
wiriim <51820876+wiriim@users.noreply.github.com>
2024-12-15 16:32:00 +0700

    Randomize hover color & increasing opacity
    Randomize the hover color, removed the borders, and increasing the
    opacity of the background by 10% every interaction

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let dimension = 16;
+let dimensions = 16;
 
 function resize(){
     let size = +prompt("What dimension you'd like to have? (max: 100)");
@@ -10,8 +10,12 @@ function resize(){
 }
 
 function addHover(div){
-    div.addEventListener("mouseover", () =>{
-        div.style.background = "green";
+    div.addEventListener("mouseover", (e) =>{
+        let opacity = e.target.style.opacity;
+        div.style.background = `rgba(${Math.floor(Math.random()*256)}, 
+        ${Math.floor(Math.random()*256)}, 
+        ${Math.floor(Math.random()*256)})`;
+        div.style.opacity  = `${+opacity+0.1}`
     });
 }
 
@@ -25,6 +29,8 @@ function removeSquares(){
 }
 
 function createSquares(dimension){
+    dimensions = dimension;
+    refreshLabel();
     const container = document.querySelector(".container");
     const containerWidth = container.clientWidth;
     const containerHeight = container.clientHeight;
@@ -33,9 +39,25 @@ function createSquares(dimension){
         div.classList.add("square");
         div.style.minWidth = containerWidth/dimension + "px";
         div.style.minHeight = containerHeight/dimension + "px";
+        div.style.background = "rgba(0,0,0)";
+        div.style.opacity = 0;
         container.appendChild(div);
         addHover(div);
     }
+}
+
+function refreshLabel(){
+    const body = document.querySelector("body");
+    const container = document.querySelector(".container");
+    if (body.contains(document.querySelector("h4"))){
+        body.removeChild(document.querySelector("h4"));
+    }
+
+    const dimensionLabel = document.createElement("h4");
+    dimensionLabel.style.marginLeft = "10vw"
+    dimensionLabel.textContent = `${dimensions}x${dimensions}`
+    body.insertBefore(dimensionLabel, container);
+    body.insertBefore
 }
 
 window.addEventListener("resize", () =>{
@@ -45,4 +67,4 @@ window.addEventListener("resize", () =>{
 const button = document.querySelector("button");
 button.addEventListener("click", resize);
 
-createSquares(dimension);
+createSquares(dimensions);
