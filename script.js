77b5cf87fdc1f766ef77f1e3d4759d86af8fb877
wiriim <51820876+wiriim@users.noreply.github.com>
2024-12-15 15:37:08 +0700

    Add button hover color & resize button to the page
    Added button color on hover, and a resize dimension for the square's
    dimension

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 let dimension = 16;
 
+function resize(){
+    let size = +prompt("What dimension you'd like to have? (max: 100)");
+    if (size > 100){
+        return alert ("The number you chose is too high");
+    }
+    removeSquares();
+    createSquares(size);
+}
+
+function addHover(div){
+    div.addEventListener("mouseover", () =>{
+        div.style.background = "green";
+    });
+}
+
+function removeSquares(){
+    const container = document.querySelector(".container");
+    const divs = document.querySelectorAll(".container div");
+
+    divs.forEach(div => {
+        container.removeChild(div);
+    });
+}
+
 function createSquares(dimension){
     const container = document.querySelector(".container");
-    console.log(container)
     const containerWidth = container.clientWidth;
-    console.log(containerWidth );
     const containerHeight = container.clientHeight;
     for (let i = 0; i < dimension*dimension; i++){
         const div = document.createElement("div");
@@ -12,7 +34,15 @@ function createSquares(dimension){
         div.style.minWidth = containerWidth/dimension + "px";
         div.style.minHeight = containerHeight/dimension + "px";
         container.appendChild(div);
+        addHover(div);
     }
 }
+
+window.addEventListener("resize", () =>{
+    location.reload();
+});
+
+const button = document.querySelector("button");
+button.addEventListener("click", resize);
 
 createSquares(dimension);
